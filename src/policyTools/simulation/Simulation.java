a16cf440f5b98aea56ce5d3e901bcf9535a4931e
obendavi <obendavi@timebon>
2012-09-20 19:03:25 +0200

    update

--- src/policyTools/simulation/Simulation.java
+++ src/policyTools/simulation/Simulation.java
@@ -37,38 +37,4 @@ public abstract class Simulation {
 	
 	public abstract void connectUsers();
 	
-<<<<<<< HEAD
-	public abstract void activateUsersRoles();
-=======
-	public static void main(String[] args) {
-		System.out.println("START SIMULATION");
-		int numberOfIteration = 10;
-		int variationSizeUsers = 1;
-		
-		double[] executionTime = new double[numberOfIteration]; 
-		
-		PolicyTextualEditor editor2 = new PolicyTextualEditor(false);
-		CommandLoadASMSSmall loadModelExample2 = new CommandLoadASMSSmall(editor2, "loadME", "loadME");
-		loadModelExample2.execute2(4,4);		
-		PolicyEditor pe = new PolicyEditor(editor2.policy);
-		System.out.println("policy rules : " + pe.getNumberPolicyRules());
-		
-		
-		for(int size =0;size <variationSizeUsers;size++){
-			for(int i =0;i<numberOfIteration; i++){	
-				Chrono c = new Chrono();
-				c.start();
-				PolicyTextualEditor editor = new PolicyTextualEditor(false);
-				//load policy model example
-				CommandLoadASMSSmall loadModelExample = new CommandLoadASMSSmall(editor, "loadME", "loadME");
-				loadModelExample.execute2(10*(size+1), 10);		
-				//load types for the simulation
-				editor.simulation.loadTypes();
-				//listen architectural model
-				editor.simulation.kevoreeListener.listen();
-				//init architectural changes
-				editor.simulation.initSimulationArchitecturalChanges();
->>>>>>> 6d9271deaf03cbdb0a2eea2445767016fa404148
-
-	
 }
